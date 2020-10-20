3807a7eb73b539d18aa2ed4d638c0c353d4a6c71
gopheramit <amitagrawal10@outlook.com>
2020-10-20 19:00:50 +0530

    trying pill requests

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(Center(
     child: Text(
-      "Hello World! Trying to create  branchescd  hi",
+      "Hello World! ",
       textDirection: TextDirection.ltr,
     ),
   ));
