ab09c44c5a532a763e5a3e43b469a1a652419508
gopheramit <gopheramit@pop-os.localdomain>
2020-10-20 18:43:05 +0530

    hmm

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    Center(child: Text(
-      "Hello World",
+void main() {
+  runApp(Center(
+    child: Text(
+      "Hello World! Trying to create branchescd ",
       textDirection: TextDirection.ltr,
-    ),)
-  );
+    ),
+  ));
 }
